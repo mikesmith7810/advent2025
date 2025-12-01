98e2d2b0b24b34c7550b8e98ed7b4e2f318927de
mikesmith-createfuture <mike.smith@xdesign.com>
2025-12-01 11:54:21 +0000

    Day 1 Part 2

--- src/main/java/day1/part2/com/mike/Main.java
+++ src/main/java/day1/part2/com/mike/Main.java
@@ -1,4 +1,4 @@
-package day1.part1.com.mike;
+package day1.part2.com.mike;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,12 +11,13 @@ public class Main {
     public static final String INPUT_TEST_FILE_LOCATION = "/Users/mikesmith/code/personal/advent2025/src/main/resources/testday1input.txt";
     public static final String INPUT_TEST_MIKE_FILE_LOCATION = "/Users/mikesmith/code/personal/advent2025/src/main/resources/testmikeday1input.txt";
 
+    static AtomicInteger count = new AtomicInteger(0);
+
     public static void main(String[] args) throws IOException {
 
         var inputs = Files.readAllLines(Path.of(INPUT_FILE_LOCATION));
 
         AtomicInteger currentPosition = new AtomicInteger(50);
-        AtomicInteger count = new AtomicInteger(0);
 
         inputs.forEach(input -> {
             var direction = input.substring(0, 1);
@@ -35,6 +36,7 @@ public class Main {
         if (direction.equalsIgnoreCase("L")) {
             for (int i = 0; i < clicks; i++) {
                 currentPosition--;
+                if (currentPosition == 0 && i + 1 != clicks) count.getAndIncrement();
                 if (currentPosition == -1) currentPosition = 99;
             }
             return currentPosition;
@@ -43,7 +45,9 @@ public class Main {
         if (direction.equalsIgnoreCase("R")) {
             for (int i = 0; i < clicks; i++) {
                 currentPosition++;
+
                 if (currentPosition == 100) currentPosition = 0;
+                if (currentPosition == 0 && i + 1 != clicks) count.getAndIncrement();
             }
             return currentPosition;
         }
