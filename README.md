0999f3c99994df96bb20a82a86b37827f0995ffc
mikesmith-createfuture <mike.smith@xdesign.com>
2025-12-01 10:24:02 +0000

    Initial Repo setup

---
+++ README.md
@@ -0,0 +1,6 @@
+Advent 2025!
+
+Each day has part1 and part2.
+
+Java 21 and Gradle for building.
+
---
+++ src/main/java/day1/part1/com/mike/Main.java
@@ -0,0 +1,7 @@
+package day1.part1.com.mike;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
